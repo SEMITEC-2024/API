c3e5cdf55f6dae14a5d74bd3e424acb44ba76d98
Vega-Luis <lanvega76r@gmail.com>
2024-05-03 07:29:50 -0600

    feat: adding login with jwt

--- src/controllers/account-controller.js
+++ src/controllers/account-controller.js
@@ -1,5 +1,6 @@
 const db = require("../database/connection");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // get user-account type
 const getUserType = (req, res) => {
@@ -12,39 +13,39 @@ const getUserType = (req, res) => {
 
 // get countries
 const getCountries = (req, res) => {
-    const sql = 'CALL get_country()'
+    const sql = "CALL get_country()";
     db.query(sql, (error, result) => {
-        if (error) throw error
-        res.json(result[0])
-    })
-}
+        if (error) throw error;
+        res.json(result[0]);
+    });
+};
 
 // get provinces from country
 const getProvinces = (req, res) => {
-    const sql = 'CALL get_province(?)'
+    const sql = "CALL get_province(?)";
     db.query(sql, [req.query.country_id], (error, result) => {
-        if (error) throw error
-        res.json(result[0])
-    })
-}
+        if (error) throw error;
+        res.json(result[0]);
+    });
+};
 
 // get cantons from province
 const getCantons = (req, res) => {
-    const sql = 'CALL get_cantons(?)'
+    const sql = "CALL get_cantons(?)";
     db.query(sql, [req.query.province_id], (error, result) => {
-        if (error) throw error
-        res.json(result[0])
-    })
-}
+        if (error) throw error;
+        res.json(result[0]);
+    });
+};
 
 // get institutions from country
 const getInstitutions = (req, res) => {
-    const sql = 'CALL get_institution(?)'
+    const sql = "CALL get_institution(?)";
     db.query(sql, [req.query.country_id], (error, result) => {
-        if (error) throw error
-        res.json(result[0])
-    })
-}
+        if (error) throw error;
+        res.json(result[0]);
+    });
+};
 
 // create user and save it into the database
 const createUser = (req, res) => {
@@ -63,20 +64,48 @@ const createUser = (req, res) => {
             res.send("Usuario registrado con exito");
         }
     );
-}
+};
 
-
-// gets the encrypted password from bd and compares it 
+// gets the encrypted password from bd and compares it
 const login = (req, res) => {
-    const { password, email } = req.body
-    const sql = 'CALL login(?)'
+    const { password, email } = req.body;
+    const sql = "CALL login(?)";
     db.query(sql, [email], (error, result) => {
-        if (error) throw error
-        let permission = bcrypt.compareSync(password, result[0][0].password)
-        let account_type = ""
-        if ( permission === true) account_type = result[0][0].name
-        res.json({"account_type": `${account_type}`,"permission": `${permission}`})
-    })
-}
+        if (error) throw error;
+        let permission = false;
+        let message = "Este usuario no se encuentra registrado";
+        let token = ''
+        if (result[0].length !== 0) {
+            const rows = result[0][0];
+            permission = bcrypt.compareSync(password, rows.password);
+            if (permission === true) {
+                message = "Usuario autenticado con éxito";
+                token = jwt.sign(
+                    {
+                        "user-id": `${rows.user_id}`,
+                        user_type_id: `${rows.user_type_id}`,
+                        username: `${rows.username}`,
+                        user_type_name: `${rows.user_type_name}`,
+                    },
+                    process.env.TOKEN_KEY
+                );
+            } else {
+                message = "Contraseña incorrecta";
+            }
+        }
+        res.header('auth-token', token).json({
+            permission: `${permission}`,
+            message: `${message}`,
+        });
+    });
+};
 
-module.exports = { getUserType, getCountries, getProvinces, getCantons, getInstitutions, createUser, login };
+module.exports = {
+    getUserType,
+    getCountries,
+    getProvinces,
+    getCantons,
+    getInstitutions,
+    createUser,
+    login,
+};
