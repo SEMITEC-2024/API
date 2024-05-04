c133e7a49b18b529f691b597067e00f3e2e9c0c8
Vega-Luis <lanvega76r@gmail.com>
2024-05-04 15:38:56 -0600

    feat: adding lesson routes

--- src/routes/account-routes.js
+++ src/routes/account-routes.js
@@ -1,7 +1,9 @@
 const { Router }  = require('express')
 const router = Router()
 const accountController = require('../controllers/account-controller');
+const lessonsController = require('../controllers/lessons-controller')
 
+// acount routes
 router.get('/account-type', accountController.getUserType)
 router.get('/countries', accountController.getCountries)
 router.get('/provinces', accountController.getProvinces)
@@ -10,4 +12,7 @@ router.get('/institutions', accountController.getInstitutions)
 router.post('/register', accountController.createUser)
 router.post('/login', accountController.login)
 
+// lessons routes
+router.get('/lessons', lessonsController.getLessons)
+router.get('/lesson', lessonsController.getLesson)
 module.exports =  router 
